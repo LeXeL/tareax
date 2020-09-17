e73d3b6fbf98a04f8da474fbcee4cc0286f44d78
Diego Rodriguez <lexelEZ@gmail.com>
2020-09-17 15:17:54 -0500

    Implemented firebase

--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './quasar'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./quasar";
 
-Vue.config.productionTip = false
+import firebase from "firebase/app";
+
+Vue.config.productionTip = false;
+
+const configOptions = {
+  apiKey: "AIzaSyAQxzlDgR3mY9h3Hb-JfGujaZnt5KLtw58",
+  authDomain: "tareax-bfce8.firebaseapp.com",
+  databaseURL: "https://tareax-bfce8.firebaseio.com",
+  projectId: "tareax-bfce8",
+  storageBucket: "tareax-bfce8.appspot.com",
+  messagingSenderId: "686870687578",
+  appId: "1:686870687578:web:d7da89818726e9e1124d63",
+  measurementId: "G-DYQH5TFLRJ",
+};
+
+firebase.initializeApp(configOptions);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
